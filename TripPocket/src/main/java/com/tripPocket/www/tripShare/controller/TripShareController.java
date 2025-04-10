993b79546b59326f5397a140a7d688277f7841b5
Geon Choi <choigeon8455@gmail.com>
2025-04-10 16:48:52 +0900

    수정1

--- TripPocket/src/main/java/com/tripPocket/www/tripShare/controller/TripShareController.java
+++ TripPocket/src/main/java/com/tripPocket/www/tripShare/controller/TripShareController.java
@@ -1,6 +1,5 @@
 package com.tripPocket.www.tripShare.controller;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
@@ -9,7 +8,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
