857abed34a1fd5c54825961dd0f2b2b97b19bef2
TaeJong <dbxowhdsla12@naver.com>
2025-04-14 15:39:34 +0900

    contextPath 경로 수정

--- TripPocket/src/main/java/com/tripPocket/www/member/controller/MemberController.java
+++ TripPocket/src/main/java/com/tripPocket/www/member/controller/MemberController.java
@@ -48,17 +48,18 @@ public class MemberController {
 	      MemberDTO member = memberService.login(memberDTO);
 	      HttpSession session = request.getSession();
 	      PrintWriter out = response.getWriter();
+	      String contextPath = request.getContextPath();
 	      if(member != null ) {
 	         session.setAttribute("isLogin",true);
 	         session.setAttribute("member", member);
 	         out.write("<script>");
 	         out.write("alert('로그인에 성공했습니다');");
-	         out.write("location.href='/www/main.do';");
+	         out.write("location.href='" + contextPath + "/main.do';");
 	         out.write("</script>");
 	      }else {
 	         out.write("<script>");
 	         out.write("alert('로그인에 실패했습니다');");
-	         out.write("location.href='/www/member/loginForm.do';");
+	         out.write("location.href='" + contextPath + "/member/loginForm.do';");
 	         out.write("</script>");
 	      }
 	      return null;
@@ -107,10 +108,11 @@ public class MemberController {
 		PrintWriter out = response.getWriter();
 		memberService.modMember(memberDTO);
 		MemberDTO member = memberService.update(memberDTO);
+		String contextPath = request.getContextPath();
 		session.setAttribute("member", member);
 		out.write("<script>");
 		out.write("alert('변경이 완료되었습니다');");
-		out.write("location.href='/www/member/mypage.do';");
+		out.write("location.href='" + contextPath + "/member/mypage.do';");
 		out.write("</script>");
 		return null;
 	}
@@ -121,9 +123,10 @@ public class MemberController {
 	    memberService.delMemberWithDependencies(memberId); // 연관 삭제 포함
 	    request.getSession().invalidate();
 	    PrintWriter out = response.getWriter();
+	    String contextPath = request.getContextPath();
 	    out.write("<script>");
 	    out.write("alert('탈퇴 완료되었습니다');");
-	    out.write("location.href='/www/main.do';");
+	    out.write("location.href='" + contextPath + "/main.do';");
 	    out.write("</script>");
 	    return null;
 	}
